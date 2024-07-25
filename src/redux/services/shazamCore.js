4b6a1eb4aa65cbfe3fd489bcdb19e99f2fcd748b
pruthviraj0106 <118849141+pruthviraj0106@users.noreply.github.com>
2024-07-25 16:14:38 +0530

    music website

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,57 @@
+
+
+
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': '2b84dd85f6msh197e8fb1ba77e66p148c28jsnd4213421142d',
+//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+//     }
+//   };
+  
+//   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+
+
+
+
+// myapi final 
+
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '2b84dd85f6msh197e8fb1ba77e66p148c28jsnd4213421142d');
+            return headers; 
+        }
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+        getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }), // Removed extra parentheses here
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+        getSongsByCountry:builder.query({ query: (countrycode) => `/charts/country?country_code=${countrycode}`}),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    }),
+});
+
+export const {
+    useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+} = shazamCoreApi;
+
+
+
